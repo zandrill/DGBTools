a6a8994566f3e504a628ef9c6d55f875745f46f2
Christoffer Rasmussen <christoffer.rasmussen@gmail.com>
2017-03-01 21:42:36 +0100

    Word læsnings script
    Dette er starten på modulet til at oprette nye medarbejdere i DGB

---
+++ dgbtool.psm1
@@ -0,0 +1,142 @@
+﻿<#
+.Synopsis
+   This reads the Word dokument that is used to gather information about new employes 
+.DESCRIPTION
+   Denne Cmdlet kan trække information ud af word dokumentet "Ny bruger Skema" og Laver et custum Objekt
+   der hedder DGBUserData, som bliver brugt af andre DGBTools Cmdlets. "Get-DGBSkema" har -Path som en Parameter,
+   det er en påkrævet parameter. 
+.EXAMPLE
+    Get-DGBSkema -Path "F:\Chris Test.docx"
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-DGBSkema{
+    [CmdletBinding()]
+    
+    Param(
+        # Sti til dokumentet "Ny Medarbejder Skema"
+        [Parameter(ValueFromPipelineByPropertyName=$true, Position=0, Mandatory=$true)]
+        $Path
+    )#Param
+
+    Begin{
+        # Laver et COM objekt for Microsoft Word
+        $objWord = New-Object -Com Word.Application
+        $objWord.Visible=$false
+    }#Begin
+
+    Process{
+        #Open current document
+        $objDocument = $objWord.Documents.Open("$Path")
+         
+        # Finder versionen af Dokumentet
+        $paras = $objDocument.Paragraphs
+        $Version = $paras[3].range.text
+        Write-Verbose "Dokumentet er $Version"
+
+        #Set variable for tables
+        $tables = $objDocument.Tables
+  
+        # Henter data fra Medarbejder delen af Ny ansat dokumentet
+        Write-Verbose "Collecting user info."
+        $Fornavne = $tables[1].cell(2,2).range.text
+        $Efternavn = $tables[1].cell(2,3).range.text
+        $Addresse = $tables[1].cell(4,2).range.text
+        $Postnummer = $tables[1].cell(4,3).range.text
+        $By = $tables[1].cell(4,4).range.text
+        $Telefonnummer = $tables[1].cell(6,3).range.text
+        $Email = $tables[1].cell(6,4).range.text
+
+        # Henter data fra Afdelingslede delen af Ny ansat dokumentet
+        Write-Verbose "Collecting Department info."
+        $Beskrivelse = $tables[2].cell(3,2).range.text
+        $Afdelingsnummer = $tables[2].cell(3,3).range.text
+        $Afdeling = $tables[2].cell(3,4).range.text
+        $Leder = $tables[2].cell(3,5).range.text
+        $StartDato = $tables[2].cell(5,4).range.text
+
+        # Henter data fra Administration/HR delen af Ny ansat dokumentet
+        Write-Verbose "Collecting HR info."
+        $Brugernavn = $tables[4].cell(2,2).range.text
+        $Personalenummer = $tables[4].cell(2,3).range.text
+
+        # Splatting the properties for the custom object
+        $Property = [ordered]@{
+            'Dokumentversion'="$Version";
+            'GivenName'="$Fornavne";
+            'Surname'="$Efternavn";
+            'StreetAddress'="$Addresse";
+            'PostalCode'="$Postnummer"
+            'City'="$By"
+            'HomePhone'="$Telefonnummer"
+            'EmailAddress'="$Email"
+            'Name'="$Brugernavn";
+            'EmployeeNumber'="$Personalenummer";
+            'Title'="$Beskrivelse";
+            'Description'="$Beskrivelse"
+            'DepartmentID'="$Afdelingsnummer";
+            'Department'= "$Afdeling"
+            'Manager'="$Leder";
+            'Startdato'="$StartDato"
+            'Company'= 'Den Gamle By'
+            'Country'="DK"
+        }# $Property
+
+        $DGBUserData = New-Object -TypeName PSObject -Property $Property
+
+        Write-Output $DGBUserData
+
+        $objDocument.ActiveDocument.Close
+
+    }# Process
+
+    End{
+        $objWord.Quit()
+    }#End
+}#Get-DGBSkema
+
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Optimize-DGBUserData{
+    [CmdletBinding()]
+    
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $DGBUserData  
+    )
+
+    Begin
+    {
+    }
+    Process{
+        if ($DGBUserData -eq 'Klik her.')
+        {
+            $DGBUserData = $false
+        }
+        Else{}
+        Write-Output $test
+    }
+    End
+    {
+    }
+}
+
+
+Send-MailMessage -From IT@dengamleby.dk -To IT@dengamleby.dk -Subject "Forkert version!" -SmtpServer dgbexch01.dengamleby.dk -Body "Medarbejder skemaet var en forkert version, dokumentets navn er TEST"
+
+
+$objDocument.ActiveDocument.Close
+$objWord.Quit()
